9692f22bc9643f148950767704c23acce7bc8e39
Administrator <Administrator@192.168.1.5>
2019-02-13 17:02:33 +0800

    123

--- src/main/java/com/selenum/handler/AuCj3Handler.java
+++ src/main/java/com/selenum/handler/AuCj3Handler.java
@@ -334,6 +334,15 @@ public class AuCj3Handler {
 					continue;
 				} catch (Exception e) {System.out.println("no - {} " +  as);}
 				
+				as = "Win a year’s supply of premium wine from Wine Selectors valued at over $1000. Simply tell us who’s wine advice would you value the most?";
+				try {
+					Select select = new Select(driver.findElementByXPath("//*[@id=\"question-6495\"]/div[2]/div[2]/select"));
+					select.selectByIndex(getNumberHas0(select.getAllSelectedOptions().size()));
+					System.out.println("yes - {} " +  as);
+					Thread.sleep(10000);
+					continue;
+				} catch (Exception e) {System.out.println("no - {} " +  as);}
+				
 				as = "//Want access to the BEST DEALS on worldwide cruises & travel?";
 				try {
 					driver.findElementByXPath("//*[@id=\"question-3284\"]/div[2]/ul/li["+ getNumber(7) + "]/label/span").click();
