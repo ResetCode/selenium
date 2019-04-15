3c9d67f006d50def9114942b2940f950fa03c380
Administrator <Administrator@USER-20190131AX>
2019-04-15 17:34:48 +1000

    123

--- src/main/java/com/selenum/handler/AuCj3Handler.java
+++ src/main/java/com/selenum/handler/AuCj3Handler.java
@@ -34,8 +34,8 @@ public class AuCj3Handler {
 			driver.get(offerUrl);
 			Thread.sleep(30000);
 			
-			try {
-				driver.findElementByXPath("//*[@id=\"page1\"]/div[2]/div[1]/div/div[2]/div/div[3]").click();
+			try {							 
+				driver.findElementByXPath("//*[@id=\"page1\"]/div[2]/div[2]/div/button").click();
 				Thread.sleep(3000);
 			} catch (Exception e) {}
 			
